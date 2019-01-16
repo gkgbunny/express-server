bd08987a6c092c553c5bf567397a9fe14db64a86
gkgbunny <46670394+gkgbunny@users.noreply.github.com>
2019-01-16 18:24:26 +0530

    Update eql-triangle.js

--- extra/patterns/eql-triangle.js
+++ extra/patterns/eql-triangle.js
@@ -1,14 +1,13 @@
-function eql(n){
+function eql(n) {
     console.log('Print an equilateral triangle with 10 rows');
-    for(let i=1;i<=n;i++)
-    {
+    for(let i=1;i<=n;i++) {
         let a=''
-        for(let j=1;j<=n-i;j++){
+        for(let j=1;j<=n-i;j++) {
             a += ' '
         }
         
         let b=''
-        for(let k=1;k<=i;k++){
+        for(let k=1;k<=i;k++) {
             b += '* '
         }
         console.log(a,b)
