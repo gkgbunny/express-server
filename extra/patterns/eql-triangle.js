a3aca62738b6b72054c9104e5c8e9c07e1590f93
gkgbunny <gaurav.kumar@successive.tech>
2019-01-16 18:39:47 +0530

    docs: update diamond.js eql-triangle.js file in extra/patterns/ folder(#39515)

--- extra/patterns/eql-triangle.js
+++ extra/patterns/eql-triangle.js
@@ -1,17 +1,16 @@
-function eql(n){
-    console.log('Print an equilateral triangle with 10 rows');
-    for(let i=1;i<=n;i++)
-    {
-        let a=''
-        for(let j=1;j<=n-i;j++){
-            a += ' '
-        }
-        
-        let b=''
-        for(let k=1;k<=i;k++){
-            b += '* '
-        }
-        console.log(a,b)
+function eql(n) {
+  console.log("Print an equilateral triangle with 10 rows");
+  for (let i = 1; i <= n; i++) {
+    let a = "";
+    for (let j = 1; j <= n - i; j++) {
+      a += " ";
     }
+
+    let b = "";
+    for (let k = 1; k <= i; k++) {
+      b += "* ";
+    }
+    console.log(a, b);
+  }
 }
-eql(10)
+eql(10);
