af11a41cbae2fbe88ff3a746114e1c2f2a204832
gkgbunny <gaurav.kumar@successive.tech>
2019-01-23 17:57:09 +0530

    docs: create folder files implement middleware(#39522)

--- src/Server.ts
+++ src/Server.ts
@@ -1,22 +1,36 @@
 import * as express from "express";
 import { IConfig } from "./config/IConfig";
+import * as bodyParser from 'body-parser';
+import notFoundRoute from './libs/routes/notFoundRoute';
+import errorHandler from './libs/routes/errorHandler';
+import { log } from "util";
 export default class Server {
   private app: express.Express;
+  bodyParser = require('body-parser')
 
   public constructor(private config: IConfig) {
     this.app = express();
   }
 
+  public initBodyParser() {
+    const { app } = this;
+    app.use(bodyParser.urlencoded({ extended: false }));
+    app.use(bodyParser.json());
+  }
+
   public bootstrap() {
+    this.initBodyParser();
     this.setupRoutes();
     return this;
   }
 
   public setupRoutes() {
     const { app } = this;
-    app.get("/health-check", (req, res) => {
-      res.send("I am OK");
+    app.get('/health-check', (req, res, next) => {
+      res.send("I am Ok");
     });
+    app.use(notFoundRoute);
+    app.use(errorHandler);
   }
 
   public run() {
---
+++ src/libs/routes/notFoundRoute.ts
@@ -0,0 +1,9 @@
+export default function notFoundRoute(req, res,next) {
+  console.log("welcome");
+  next({
+    error: "Not Found",
+    message: "error",
+    status: 500,
+    timestamp: new Date()
+    });
+}
