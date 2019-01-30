93fd660092a08e776bf03e02f1f2a87ada12f82b
gkgbunny <gaurav.kumar@successive.tech>
2019-01-30 13:48:47 +0530

    feat: implement authentication and authorization(#40009)

--- src/controllers/trainee/routes.ts
+++ src/controllers/trainee/routes.ts
@@ -3,7 +3,6 @@ import Controller from "./Controller";
 import { authMiddleware } from "../../libs/routes";
 const controller = new Controller();
 export const traineeRouter: Router = Router();
-<<<<<<< HEAD
 traineeRouter.get("/", authMiddleware("Trainee", "read"), controller.retrieve);
 traineeRouter.post("/", authMiddleware("Trainer", "write"), controller.create);
 traineeRouter.put(
@@ -14,15 +13,5 @@ traineeRouter.put(
 traineeRouter.delete(
   "/:id",
   authMiddleware("Trainer", "delete"),
-=======
-import validate from "../validate";
-import { validateHandler } from "../../libs/routes";
-traineeRouter.get("/", validateHandler(validate.get), controller.retrieve);
-traineeRouter.post("/", validateHandler(validate.create), controller.create);
-traineeRouter.put("/", validateHandler(validate.update), controller.update);
-traineeRouter.delete(
-  "/:id",
-  validateHandler(validate.delete),
->>>>>>> develop
   controller.delete
 );
--- src/libs/routes/index.ts
+++ src/libs/routes/index.ts
@@ -1,10 +1,5 @@
 import successHandler from "./successHandler";
 import errorHandler from "./errorHandler";
 import notFoundRoute from "./notFoundRoute";
-<<<<<<< HEAD
 import authMiddleware from "./authMiddleware";
 export { successHandler, errorHandler, notFoundRoute, authMiddleware };
-=======
-import validateHandler from "./validateHandler";
-export { successHandler, errorHandler, notFoundRoute, validateHandler };
->>>>>>> develop
