3cc3461d2a567cf8a8dcb9acc751567758472934
gkgbunny <gaurav.kumar@successive.tech>
2019-02-06 18:06:40 +0530

    feat: updated old files(#40014)

--- src/controllers/user/Controller.ts
+++ src/controllers/user/Controller.ts
@@ -7,7 +7,9 @@ import UserRepo from '../../repositories/user/UserRepository';
 export default class Controller {
   public post(req: Request, res: Response, next) {
     console.log('Post Method');
+    console.log(req.body);
     new UserRepo().findone(req.body).then((data) => {
+      console.log(data);
       const token = jwt.sign(data.toJSON(), configuration.key, { expiresIn: 60 * 15 });
       res.status(200).send(successHandler('Created token is', token, 200));
     });
